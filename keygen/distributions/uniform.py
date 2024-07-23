7483d52bc6c9673c84e0a320593ff4047d8f99f6
Alexandros Ionitsa <alexandros.ionitsa@gmail.com>
2024-07-23 21:17:35 +0300

    Enhance key distribution generator

--- keygen/distributions/uniform.py
+++ keygen/distributions/uniform.py
@@ -3,5 +3,16 @@ from .base import Distribution
 
 
 class UniformDistribution(Distribution):
+    """Uniform distribution class for generating keys.
+    """
+
     def generate(self, arrival_rate):
+        """Generate keys based on a uniform distribution.
+
+        Args:
+            arrival_rate (int): The number of keys to generate.
+
+        Returns:
+            list: A list of keys chosen uniformly at random.
+        """
         return random.choices(self.keys, k=arrival_rate)
