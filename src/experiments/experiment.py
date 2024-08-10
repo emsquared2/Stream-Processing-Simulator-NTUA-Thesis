74a839060e2919de1b06ce9125ba82904bd474df
Manos Emmanouilidis <manos.emmanouilidis05@gmail.com>
2024-08-10 17:09:34 +0300

    Implement module for performing experiments

---
+++ src/experiments/experiment.py
@@ -0,0 +1,78 @@
+from keygen.KeyGenerator import KeyGenerator
+from simulator.Simulator import Simulator
+from utils.utils import load_steps_from_file, load_config
+
+
+def update_config(config, updates):
+    """
+    Update the configuration dictionary with the given updates.
+
+    Args:
+        config (dict): The original configuration dictionary.
+        updates (dict): A dictionary of parameters to update.
+
+    Returns:
+        dict: The updated configuration dictionary.
+    """
+    for key, value in updates.items():
+        if key in config["node"]:
+            config["node"][key] = value
+        elif key in config["simulator"]:
+            config["simulator"][key] = value
+        elif key in config["keygen"]:
+            config["keygen"][key] = value
+        else:
+            print(f"Warning: Unrecognized configuration parameter '{key}'")
+
+    return config
+
+
+def run_experiment(config_file, output_file, **kwargs):
+    """
+    Run an experiment with specific parameters modified from the config file.
+
+    Args:
+        config_file (str): Path to the configuration file.
+        output_file (str): Prefix for the output files where the generated keys will be saved.
+        **kwargs: Parameters to be modified in the configuration file.
+    """
+    # Load the configuration configuration
+    config = load_config(config_file)
+
+    # Modify the configuration based on kwargs
+    config = update_config(config, kwargs)
+
+    # Generate the key streams using the updated configuration
+    keygen = KeyGenerator(config["keygen"])
+    keygen.generate_input(output_file)
+
+    # Initialize the simulator with the modified configuration
+    num_nodes = config["simulator"]["number_of_nodes"]
+    strategy_name = config["simulator"]["strategy"]["name"]
+    strategy_params = {
+        key: value for key, value in config["simulator"]["strategy"].items()
+    }
+    window_size = config["node"]["window_size"]
+    slide = config["node"]["slide"]
+    throughput = config["node"]["throughput"]
+    complexity_type = config["node"]["complexity_type"]
+
+    # Initialize the simulator
+    simulator = Simulator(
+        num_nodes,
+        strategy_name,
+        window_size,
+        slide,
+        throughput,
+        complexity_type,
+        strategy_params,
+    )
+
+    # Read steps data from all generated files
+    steps_data = []
+    for i in range(config["keygen"]["streams"]):
+        stream_file = f"{output_file}{i}"
+        steps_data.extend(load_steps_from_file(stream_file))
+
+    # Run the simulation with the modified configuration
+    simulator.sim(steps_data)
