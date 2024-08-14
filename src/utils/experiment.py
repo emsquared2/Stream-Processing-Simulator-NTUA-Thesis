4994f5585d71f89443118cda6d77b8cdd802fffe
Manos Emmanouilidis <manos.emmanouilidis05@gmail.com>
2024-08-14 21:04:04 +0300

    Update Experiments

--- src/utils/experiment.py
+++ src/utils/experiment.py
@@ -2,6 +2,7 @@ from keygen.KeyGenerator import KeyGenerator
 from simulator.Simulator import Simulator
 from utils.utils import load_steps_from_file, load_config, update_config
 
+
 def run_experiment(config_file, output_file, extra_dir=None, **kwargs):
     """
     Run an experiment with specific parameters modified from the config file.
@@ -23,23 +24,17 @@ def run_experiment(config_file, output_file, extra_dir=None, **kwargs):
 
     # Initialize the simulator with the modified configuration
     num_nodes = config["simulator"]["number_of_nodes"]
+    topology = config["topology"]
     strategy_name = config["simulator"]["strategy"]["name"]
     strategy_params = {
         key: value for key, value in config["simulator"]["strategy"].items()
     }
-    window_size = config["node"]["window_size"]
-    slide = config["node"]["slide"]
-    throughput = config["node"]["throughput"]
-    complexity_type = config["node"]["complexity_type"]
 
     # Initialize the simulator
     simulator = Simulator(
         num_nodes,
+        topology,
         strategy_name,
-        window_size,
-        slide,
-        throughput,
-        complexity_type,
         strategy_params,
         extra_dir,
     )
